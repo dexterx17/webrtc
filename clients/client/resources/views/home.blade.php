641fbb600653a73547328a49b3134b67d6a78392
Jaime Santana <sistemas@santana.ec>
2018-02-16 17:38:10 +0000

    cliente web de websocket

--- clients/client/resources/views/home.blade.php
+++ clients/client/resources/views/home.blade.php
@@ -1,1 +1,164 @@
 @extends('template.base')
+
+@section('content')
+
+<main class="mn-inner inner-active-sidebar">
+    <div class="middle-content">
+        <div class="row no-m-t no-m-b">
+            <div class="col s12 m12 l6">
+                <div class="card stats-card">
+                    <div class="card-content">
+                        <div class="card-options">
+                            <ul>
+                                <li><a href="javascript:void(0)"><i class="material-icons">more_vert</i></a></li>
+                            </ul>
+                        </div>
+                        <span class="card-title">Plataformas
+                        </span>
+                        <span class="stats-counter"><span class="counter" id="numero_plataformas">0</span><small>conectados</small></span>
+                    </div>
+                    <div id="sparkline-bar"></div>
+                </div>
+            </div>
+            <div class="col s12 m12 l6">
+                <div class="card stats-card">
+                    <div class="card-content">
+                        <div class="card-options">
+                            <ul>
+                                <li><a href="javascript:void(0)"><i class="material-icons">more_vert</i></a></li>
+                            </ul>
+                        </div>
+                        <span class="card-title">Controladores</span>
+                        <span class="stats-counter"><span class="counter" id="numero_controladores">0</span><small>conectados</small></span>
+                    </div>
+                    <div id="sparkline-line"></div>
+                </div>
+            </div>
+        </div>
+        <div class="row no-m-t no-m-b">
+            <div class="col s12 m12 l8">
+                <div class="card visitors-card">
+                    <div class="card-content">
+                        <div class="card-options">
+                            <ul>
+                                <li><a href="javascript:void(0)" class="card-refresh"><i class="material-icons">refresh</i></a></li>
+                            </ul>
+                        </div>
+                        <span class="card-title">Datos transmitidos<span class="secondary-title">Mensajes de estado vs Mensajes de control</span></span>
+                        <div id="flotchart1"></div>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m12 l4">
+                <div class="card server-card">
+                    <div class="card-content">
+                    <div class="card-options">
+                        <ul>
+                            <li class="red-text"><span class="badge red lighten-3" id="status_server">offline</span></li>
+                        </ul>
+                    </div>
+                        <span class="card-title">Servidor</span>
+                        <div class="server-load row">
+                            <div class="server-stat col s6">
+                                <p id="numero_clientes">0</p>
+                                <span>Clientes</span>
+                            </div>
+                            <div class="server-stat col s6">
+                                <p id="total_mensajes">0</p>
+                                <span>Mensajes</span>
+                            </div>
+                        </div>
+                        <div id="flotchart2"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @include('template.partials.sidebar')
+
+</main>
+
+@endsection
+
+@section('js')
+
+<script type="text/javascript">
+
+    function section_cliente(data){
+        var icon = 'hearing';
+        if(data.connected === 'controlador')
+            icon = 'settings_remote';
+        if(data.connected === 'plataforma')
+            icon = 'my_location';
+        
+        var section ='<div  class="info-item message-item" id="cliente' + data.ip + '"><i class="small material-icons">'+icon;
+        section += '</i><div class="message-info"><div class="message-author">' + data.connected + '</div>';
+        section += '<small>' + data.ip + '</small></div></div>';
+        return section;
+    }
+    function WebSocketTest()
+    {
+        if ("WebSocket" in window)
+        {
+            Materialize.toast('WebSocket is supported by your Browser and online!', 4000);
+            // Let us open a web socket
+            var ws = new WebSocket("ws://10.211.159.62:9000/");
+            
+            ws.onopen = function()
+            {
+                $('#status_server').html('online').removeClass('red').addClass('green');
+                // Web Socket is connected, send data using send()
+                //ws.send('{ "cliente" : "visualizador" }');
+                ws.send('visualizador');
+                Materialize.toast('Visualizador inicializado', 4000);
+            };
+            
+            ws.onmessage = function (evt) 
+            { 
+                var data = evt.data;
+
+                //console.log('- - - - -- - - - - - - -');
+                //console.log(data);
+                //console.log('********************');
+
+                var json = JSON.parse(data);
+
+                //console.log(json);
+                //console.log('- - - - -- - - - - - - -');
+
+                if( typeof json['n_clientes'] !== "undefined" ){
+                    $('#numero_clientes').html(json['n_clientes']);
+                    $('#total_mensajes').html(json['n_mensajes']);
+                }else if(typeof json['connected'] !== "undefined") {
+                    $('#list-clientes-conectados').append(section_cliente(json));
+                }else if(typeof json['disconnected'] !== "undefined") {
+                    $item = $('#list-clientes-conectados').children('[id^="cliente'+json.ip+'"]').fadeOut('slow');
+                }else{
+                    console.log(json);
+                }
+            };
+            
+            ws.onclose = function()
+            { 
+                $('#status_server').html('offline').removeClass('green').addClass('red');
+              // websocket is closed.
+              Materialize.toast('Visualizador desconetado del WebSocket!', 4000);
+            };
+                
+           window.onbeforeunload = function(event) {
+              ws.close();
+           };
+        }
+        
+        else
+        {
+           // The browser doesn't support WebSocket
+           Materialize.toast('WebSocket NOT supported by your Browser!', 4000);
+           $('#status_server').html('offline').removeClass('green').addClass('red');
+        }
+    }
+
+    setTimeout(function(){ WebSocketTest() }, 4000);
+</script>
+@endsection
--- servers/directo.js
+++ servers/directo.js
@@ -19,11 +19,82 @@ var puertoServer = 9000;
 //Instancias para clientes del websocket
 var plataforma;
 var controlador;
+var visualizadores = [];
+
+var n_clientes = 0;
+var n_mensajes = 0;
 
 //Creamos y configuramos el WebSocket
 var server = ws.createServer(function(conexion){
 	console.log('Nuevo cliente conectado');
+	//console.log(server);
+	//console.log(server.socket);
+	//aumento un 1 el contador de clientes
+	n_clientes++;
+
+	//console.log('-------------------------------------');
+	//console.log(conexion.socket.remoteAddress);
+	//console.log('----');
+	//console.log(conexion.remoteAddress);
+	//console.log('-------------------------------------');
+
+	conexion.on("connected", function () {
+	    console.log("un connected ahi:");
+	});
+	conexion.on("connect", function () {
+	    console.log("un connect ahi:");
+	});
+	conexion.on("open", function () {
+	    console.log("un open ahi:");
+	});
+	conexion.on("connection", function () {
+	    console.log("un connection ahi sin parametro:");
+	});
+
+	conexion.on('connection', function(ws) {
+		console.log("un connection ahi:");
+	});
+
+	conexion.on("text", function (str) {
+		n_mensajes++;
+	    console.log("on mensaje:");
+	    console.log(str);
+	    console.log(conexion.tipo);
+	    if (conexion.tipo === null || ( typeof conexion.tipo === "undefined") ) {
+			conexion.tipo = str
+			//broadcast(str+" entered");
+			//broadcast(JSON.stringify('{"n_clientes":'+ n_clientes +'}'));
+			broadcast('{"n_clientes":'+ n_clientes +',"n_mensajes":' + n_mensajes + '}');
+			broadcast('{"connected":"'+ conexion.tipo +'","ip":"'+ conexion.socket.remoteAddress +'"}');
+			if(conexion.tipo==="visualizador"){
+				visualizadores.push(conexion);
+			}
+
+		} else
+			broadcast("["+conexion.tipo+"] "+str)
+	});
+
+	conexion.on("close", function (code, reason) {
+		console.log('desconectado');
+		//disminuyo en 1 el contador de clientes
+		n_clientes--;
+		broadcast('{"n_clientes":'+ n_clientes +',"n_mensajes":' + n_mensajes + '}');
+		broadcast('{"disconnected":"'+ conexion.tipo  +'","ip":"'+ conexion.socket.remoteAddress +'"}');
+	});
+
+	 conexion.on('error', function(e){
+        console.log('error');
+        console.log(e);
+    });
+	
 
 //Arrancamos el servidor WebSocket en la ip y puerto configurado al incio
 }).listen(puertoServer,ipServer);
 
+function broadcast(str) {
+	server.connections.forEach(function (connection) {
+		if(connection!=null){
+			connection.sendText(str)
+		}
+	})
+}
